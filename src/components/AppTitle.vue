53a4e4a408d19214183d65bd2ab87390778e53e3
IreneChicca <irene.chicca@outlook.com>
2023-09-07 16:26:50 +0200

    Apps

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      messaggio: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ messaggio }}</h1>
+</template>
+
+<style></style>
